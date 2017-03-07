536b83efafbdd403468f5c3e86b9d6a32c0cfff7
XY20130630 <xy20130630@gmail.com>
2017-03-07 11:38:49 +0800

    First submitted

---
+++ over-wall-tool.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+	cout<<"Welcome to over-wall-tool designed by konnyaku xzy."<<endl;
+	cout<<"Init......"<<endl;
+	system("sudo chmod +x u1604d");
+	cout<<"Starting......"<<endl;
+	system("sudo ./u1604d -M vpn");
+	return 0;
+}
